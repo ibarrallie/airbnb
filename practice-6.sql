24c92b21197c6d1d88f3066fc2ced140e3f0a6be
Alexandra Ibarra <alexandraibarra2025@u.northwestern.edu>
2024-04-08 20:02:25 +0000

    I finished practice 7

--- practice-6.sql
+++ practice-6.sql
@@ -15,7 +15,7 @@
 -- | Brighton Park          | 678      |
 -- | Burnside               | 10       |
 
-SELECT listings.neighborhood, COUNT(reviews.id)
+SELECT listings.neighborhood, COUNT(reviews.listing_id)
 FROM listings INNER JOIN reviews ON listings.id = reviews.listing_id
 GROUP BY listings.neighborhood
 ;
